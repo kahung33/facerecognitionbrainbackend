9f59f0c1ba1e56ef66c4d2e21918d917ed996b8b
雞ry牛羊雞ry牛羊 <kahung930809@gmail.com>
2022-10-17 03:17:27 -0600

    second part

--- src/App.js
+++ src/App.js
@@ -7,9 +7,10 @@ import './App.css';
 import ParticlesBg from 'particles-bg';
 import Clarifai from 'clarifai';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition'
+import SignIn from './components/SignIn/SignIn';
+import Register from './components/Register/Register';
 
-
-console.log(Clarifai);
+// console.log(Clarifai);
 
 const app = new Clarifai.App({
   apiKey: 'd37936effb2c4579a90dc12a2daf243ae9bce8df45964982ab401a67dc52efd6'
@@ -22,16 +23,18 @@ class App extends Component {
       input: '',
       imageUrl:'',
       box: {},
+      route: 'signIn',
+      isSignedIn : false, 
     }
   }
 
   calculateFaceLocation = (data) => {
     const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
-    console.log(clarifaiFace);
+    // console.log(clarifaiFace);
     const image = document.getElementById('inputImage');
     const width = Number(image.width);
     const height = Number(image.height);
-    console.log(width,height)
+    // console.log(width,height)
     return {
       leftCol: clarifaiFace.left_col * width,
       rightCol: width - (clarifaiFace.right_col * width),
@@ -41,17 +44,18 @@ class App extends Component {
   }
 
   displayFaceBox = (box) => {
-    console.log(box);
+    // console.log(box);
     this.setState({box : box});
   }
-
+ 
   onInputChange = (event) => {
     this.setState({ input : event.target.value });
   }
 
+  //Doing in way of JS REST API
   onButtonSubmit = () =>{
     this.setState({imageUrl: this.state.input})
-    console.log('calling api');
+    // console.log('calling api');
     const USER_ID = 'kahung33';
     const PAT = '2a6d757f14714eb8a9b6a13b122574ea';
     const APP_ID = 'brainrecog';
@@ -88,24 +92,46 @@ class App extends Component {
   // this will default to the latest version_id
 
   fetch("https://api.clarifai.com/v2/models/" + MODEL_ID + "/versions/" + MODEL_VERSION_ID + "/outputs", requestOptions)
+    //cast the response to Javascript Obejct
       .then(response => response.json())
       .then(result => {
-        console.log('result:', result);
+        // //use this to check how to get the bounding box
+        // console.log('result:', result);
         this.displayFaceBox(this.calculateFaceLocation(result));
       }).catch(error => console.log('error', error));
   }
 
+  onRouteChange = (route) => {
+    if (route === 'signout'){
+      this.setState({isSignedIn: false})
+    } else if (route === 'home'){
+      this.setState({isSignedIn: true})
+    }
+    this.setState({route : route});
+  }
+
   render(){
+    const { isSignedIn, imageUrl, route, box} = this.state;
     return (
       <div className="App">
-        <Navigation />
-        <Logo />
-        <Rank />
-        <ImageLinkForm 
-        onInputChange = {this.onInputChange} 
-        onButtonSubmit = {this.onButtonSubmit}/>
+        <Navigation isSignedIn={isSignedIn} onRouteChange = {this.onRouteChange}/>
         <ParticlesBg className ='particles' type='polygon' bg={true} />
-        <FaceRecognition box = {this.state.box} imageUrl = {this.state.imageUrl}/>
+        { route === 'home' 
+          ?<div>
+          <Logo />
+          <Rank />
+          <ImageLinkForm 
+          onInputChange = {this.onInputChange} 
+          onButtonSubmit = {this.onButtonSubmit}/>
+          <FaceRecognition box = {box} imageUrl = {imageUrl}/>
+          </div>
+          : (
+            route === 'signIn' ?
+            <SignIn onRouteChange={this.onRouteChange}/> 
+            : <Register onRouteChange={this.onRouteChange}/>
+          )
+        }
+        
       </div>
     );
   }
